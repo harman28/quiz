f0ae8395e9715ce517c4d4248893aafef570bc4c
Harman Singh <harman28@gmail.com>
2016-11-05 00:49:01 +0530

    Real fairness check

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source "https://rubygems.org"
+
+# This really is asking for it
+# It's a fairly comprehensive library with all sorts of statistical methods
+# All I care about is Pearson's Something Coefficient, and statsample uses
+# a bunch of methods that are all set to deprecated soon.
+# TODO: Implement Pearson's, and covariance, and std deviation yourself.
+# Can't be that hard.
+gem "statsample"
--- fairness.rb
+++ fairness.rb
@@ -1,10 +1,17 @@
+require 'statsample'
+require 'statsample/bivariate'
+require 'statsample/bivariate/pearson'
+
 class FairnessCheck
 
   ##
   # This seemed to fit the use case
   # https://en.wikipedia.org/wiki/Pearson_product-moment_correlation_coefficient
   def self.pearson_product_moment_correlation_coefficient strengths, scores
-    # Dummy value
-    return -1 + rand*(1 - -1)
+
+    x = strengths.to_vector(:scale)
+    y = scores.to_vector(:scale)
+
+    Statsample::Bivariate.pearson(x, y)
   end
 end

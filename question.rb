96b9c980327798661689bb21a4b4b3eb12eac4d4
Harman Singh <harman28@gmail.com>
2016-11-04 23:30:05 +0530

    Question class

---
+++ question.rb
@@ -0,0 +1,20 @@
+class Question
+
+  attr_reader :difficulty
+
+  @@upper_difficulty = 100
+  @@lower_difficulty = 0
+
+  def initialize lower_difficulty = 0, upper_difficulty = 100
+    @difficulty = random_difficulty(lower_difficulty, upper_difficulty+1)
+
+    @@lower_difficulty = lower_difficulty
+    @@upper_difficulty = upper_difficulty
+  end
+
+  private
+
+  def random_difficulty lower_difficulty, upper_difficulty
+    lower_difficulty + rand(upper_difficulty + 1 - lower_difficulty)
+  end
+end
